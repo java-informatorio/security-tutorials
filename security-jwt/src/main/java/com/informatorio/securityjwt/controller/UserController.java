0ed99b1627b5d2ce03a13d3425a62ab68e0d2a2d
Eduardo Ortiz <eduortiz.g@gmail.com>
2022-01-06 08:27:09 -0300

    JWT-001 - Added UserController and some DTO

--- security-jwt/src/main/java/com/informatorio/securityjwt/controller/UserController.java
+++ security-jwt/src/main/java/com/informatorio/securityjwt/controller/UserController.java
@@ -1,14 +1,20 @@
 package com.informatorio.securityjwt.controller;
 
-
+import com.informatorio.securityjwt.domain.Role;
 import com.informatorio.securityjwt.domain.User;
+import com.informatorio.securityjwt.dto.UserRoleDTO;
 import com.informatorio.securityjwt.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -20,5 +26,23 @@ public class UserController {
     @GetMapping("/users")
     public ResponseEntity<List<User>> getUsers() {
         return ResponseEntity.ok().body(userService.getUsers());
+    }
+
+    @PostMapping("/users")
+    public ResponseEntity<User> saveUser(@RequestBody User user) {
+        URI uri = URI.create(ServletUriComponentsBuilder.fromCurrentContextPath().path("/api/users").toUriString());
+        return ResponseEntity.created(uri).body(userService.saveUser(user));
+    }
+
+    @PostMapping("/roles")
+    public ResponseEntity<Role> saveRole(@RequestBody Role role) {
+        URI uri = URI.create(ServletUriComponentsBuilder.fromCurrentContextPath().path("/api/roles").toUriString());
+        return ResponseEntity.created(uri).body(userService.saveRole(role));
+    }
+
+    @PostMapping("/users/{userId}/roles")
+    public ResponseEntity<Role> saveUser(@PathVariable Long userId, @RequestBody UserRoleDTO userRoleDTO) {
+        userService.addRoleToUser(userRoleDTO.getUsername(), userRoleDTO.getRoleName());
+        return ResponseEntity.ok().build();
     }
 }
