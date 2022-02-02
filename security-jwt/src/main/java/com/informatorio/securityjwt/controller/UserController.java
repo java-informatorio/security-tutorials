09f826017f525cdfc24e8e0969a928ba7747b5ab
Eduardo Ortiz <eduortiz.g@gmail.com>
2022-02-02 19:21:34 -0300

    JWT-005 - Added JWTTokenFilter (QA roles)

--- security-jwt/src/main/java/com/informatorio/securityjwt/controller/UserController.java
+++ security-jwt/src/main/java/com/informatorio/securityjwt/controller/UserController.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -18,7 +17,6 @@ import java.net.URI;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api")
 @RequiredArgsConstructor
 public class UserController {
     private final UserService userService;
